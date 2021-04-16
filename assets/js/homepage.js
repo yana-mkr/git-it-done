f0dabb78e99715d6f7627fcc2c65f6104b25dd2e
Yana Mkrtchyan <yana@Yanas-MacBook-Pro.local>
2021-04-16 17:49:04 -0500

    able to request data and see number of issues

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -46,6 +46,21 @@ var getUserRepos = function(user) {
           // append to container
           repoEl.appendChild(titleEl);
 
+          // create a status element
+          var statusEl = document.createElement("span");
+          statusEl.classList = "flex-row align-center";
+
+          // check if current repo has issues or not
+          if (repos[i].open_issues_count > 0) {
+              statusEl.innerHTML =
+              "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + "issue(s)";
+          } else {
+              statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+          }
+
+          // append to container
+          repoEl.appendChild(statusEl);
+
           // append container to the dom
           repoContainerEl.appendChild(repoEl);
       }
